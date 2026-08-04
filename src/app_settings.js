8995095eae0543b2a2f1e8ec54de0a47d050e489
Peter Koval <koval.peter@gmail.com>
2026-08-04 22:49:56 +0200

    using local storage to save/restore visibility of the chart.

--- src/app_settings.js
+++ src/app_settings.js
@@ -1,10 +1,14 @@
 class AppSettings {
     constructor() {
-        this.is_chart_shown = Boolean(localStorage.getItem("show-chart"))
+        this.is_chart_shown = localStorage.getItem("show-chart") === "true"
     }
 
-    save_chart_visibility(){
-
+    /**
+     *
+     * @param {Boolean} is_chart_shown
+     */
+    save_chart_visibility(is_chart_shown) {
+        localStorage.setItem("show-chart", is_chart_shown ? "true" : "false")
     }
 }
 
